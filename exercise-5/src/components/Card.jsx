67ef15ce92dc206273659836d68f941b4c5f7cbe
leap <Bunleab.sok@student.cadt.edu.kh>
2025-03-06 15:54:10 +0700

    new version

--- exercise-5/src/components/Card.jsx
+++ exercise-5/src/components/Card.jsx
@@ -2,9 +2,14 @@ export default function Card({make,model,year,img}) {
     return (
     <div className="card">
       <img src={img} />
-      <h2>Make:  {make} </h2>
-      <h2>Model:{model}</h2>
-      <h2>Year:{year}</h2>
+      <div>
+        <h2>Make:  {make} </h2>
+        <h2>Model:{model}</h2>
+        <h2>Year:{year}</h2>
+      </div>
+       <div className="btn">
+        <button>View Details</button>
+       </div>
     </div>
     );
   }
